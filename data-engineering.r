6b89e4fb7a5ec9c3ddd67d68cb6d352369a27d6a
Jo Dudding <j.m.dudding@gmail.com>
2025-05-04 21:55:50 +1200

    extract sewers

--- data-engineering.r
+++ data-engineering.r
@@ -20,10 +20,10 @@ gbsb_url <- "https://en.wikipedia.org/wiki/The_Great_British_Sewing_Bee"
 
 gbsb_wiki <- read_html(gbsb_url)
 
-gbsb_tables <- html_nodes(gbsb_wiki, "table")
+gbsb_tables <- html_nodes(gbsb_wiki, ".wikitable")
 
-gbsb_overview <- gbsb_wiki  |> 
-  html_node(".wikitable") |> 
+gbsb_overview <- gbsb_tables  |> 
+  pluck(1) |> 
   html_table(fill = TRUE) |> 
   clean_names() |> 
   mutate(
@@ -38,10 +38,71 @@ series <- gbsb_overview$series
 
 gbsb_series_urls <- glue('{gbsb_url}_series_{series}')
 
+gbsb_series_wiki <- tibble(
+  series = series,
+  wiki = map(gbsb_series_urls, read_html)
+)
 
+gbsb_series_tables <- gbsb_series_wiki |> 
+  group_by(series) |> 
+  mutate(
+    table = map(
+      wiki, 
+      ~html_nodes(.x, ".wikitable") |> 
+        html_table(fill = TRUE)
+    )
+  ) |> 
+  unnest_longer(table) |> 
+  mutate(
+    table_type = case_when(
+      row_number() == 1 ~ 'sewers',
+      row_number() == 2 ~ 'elimination',
+      row_number() == n() ~ 'ratings',
+      TRUE ~ 'episodes'
+    )
+  ) |> 
+  ungroup() |> 
+  select(-wiki)
+  
 
 #--- series sewers ---
 
+get_sewers <- function(num) {
+  
+  cli_alert_info('sewers for series {num}')
+  
+  gbsb_series_tables |> 
+    filter(table_type == 'sewers') |> 
+    select(-table_type) |> 
+    filter(series == num) |> 
+    unnest(table) |> 
+    gather(-series, -2, key = 'key', value = 'value') |> 
+    rename(sewer_fullname = 2) |> 
+    mutate(
+      across(2:4, ~str_remove(.x, '\\[.*\\]')),
+      key = str_to_lower(key),
+      key = if_else(
+        key %in% c('age', 'occupation', 'placement'),
+        key,
+        'residence'
+      )
+    )
+  
+}
+
+gbsb_sewers <- map_dfr(series, get_sewers) |> 
+  spread(key = key, value = value) 
+
+gbsb_sewers |> 
+  group_by(series) |> 
+  summarise(
+    n_sewers = n(),
+    n_with_age = sum(!is.na(age)),
+    n_with_occ = sum(!is.na(occupation)),
+    n_with_res = sum(!is.na(residence)),
+    n_with_place = sum(!is.na(placement))
+  ) |> 
+  print()
 
 #--- series eliminations ---
 
@@ -51,3 +112,8 @@ gbsb_series_urls <- glue('{gbsb_url}_series_{series}')
 
 
 #--- series episodes ---
+
+
+#--- series episode themes ---
+
+
