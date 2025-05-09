3aff2ae68f6f3cf301871d92a7f79ab182691e2c
Jo Dudding <j.m.dudding@gmail.com>
2025-05-09 23:10:45 +1200

    finish viewership charts

--- viewership-charts.r
+++ viewership-charts.r
@@ -71,8 +71,17 @@ scale_y_comma <- function(
 
 #--- ggplot theme ---
 
+theme_set(
+  theme_minimal() +
+    theme(
+      plot.title.position = 'plot',
+      plot.caption.position = 'plot'
+    )
+)
 
-
+update_geom_defaults('col', aes(fill = '#9C291B', colour = NA))
+update_geom_defaults('line', aes(colour = '#39566F', linewidth = 1))
+update_geom_defaults('point', aes(colour = '#39566F', size = 2))
 
 #--- data prep ---
 
@@ -91,17 +100,30 @@ ratings <- gbsb_ratings |>
     ),
     premier = case_when(episode == min(episode) ~ total_viewers_m),
     finale = case_when(episode == max(episode) ~ total_viewers_m),
-    series_label = case_when(episode == floor(mean(episode)) ~ series)
+    series_label = case_when(episode == floor(mean(episode)) ~ series),
+    series_y = case_when(
+      episode == floor(mean(episode)) ~ 
+        coalesce(total_viewers_m, mean(total_viewers_m, na.rm = TRUE))
+    )
   ) |> 
   ungroup() 
+
+ratings_first_last <- ratings |> 
+  filter(episode_type != 'Middle') |> 
+  select(episode_type, series, total_viewers_m) |> 
+  spread(key = episode_type, value = total_viewers_m) |> 
+  mutate(
+    num_change = Finale - Premiere,
+    pct_change = num_change / Premiere,
+    label = percent(pct_change, 0.1)
+  )
 
 #--- bar chart ---
 
 ratings |> 
-  ggplot(aes(series_ep, total_viewers_m, fill = factor(series),
-    label = series_label)) +
+  ggplot(aes(series_ep, total_viewers_m, fill = factor(series))) +
   geom_col() +
-  geom_text(nudge_y = 0.5) +
+  geom_text(aes(y = series_y, label = series_label), nudge_y = 0.5) +
   scale_y_comma() +
   scale_fill_series() +
   labs(
@@ -117,6 +139,31 @@ ratings |>
 
 #--- lollipop chart ---
 
+ratings |> 
+  group_by(series) |> 
+  mutate(
+    avg_viewers = mean(total_viewers_m),
+    diff_viewers = total_viewers_m - avg_viewers
+  ) |> 
+  filter(sum(!is.na(total_viewers_m)) >= 8) |> 
+  ungroup() |> 
+  ggplot(aes(episode, total_viewers_m, yend = avg_viewers)) +
+  geom_segment(linewidth = 0.1) +
+  geom_line(aes(y = avg_viewers), linewidth = 0.1) +
+  geom_point(aes(colour = episode_type)) +
+  scale_x_continuous(breaks = 1:10) +
+  scale_y_comma(expand_min = 0.05) +
+  scale_colour_manual(values = type_pal) +
+  facet_wrap(~ series) +
+  labs(
+    x = 'Episode', y = 'Total viewers (m)',
+    title = 'Viewership seems steady across the episodes in a series',
+    subtitle = 'Total viewers (m) across episodes by series of GBSB',
+    caption = 'Only series with 8+ assessed episodes'
+  ) +
+  guides(colour = 'none')
+
+
 #--- line chart ---
 
 ratings |> 
@@ -125,7 +172,7 @@ ratings |>
   ungroup() |> 
   ggplot(aes(episode, total_viewers_m, colour = factor(series),
     label = case_when(episode_type == 'Finale' ~ series))) +
-  geom_line(linewidth = 1) +
+  geom_line() +
   geom_text(nudge_x = 0.15) +
   scale_x_continuous(breaks = 1:10) +
   scale_y_comma(expand_min = 0.05) +
@@ -147,7 +194,7 @@ ratings |>
   ungroup() |> 
   ggplot(aes(episode, total_viewers_m, colour = factor(series),
     label = case_when(episode_type == 'Finale' ~ series))) +
-  geom_line(linewidth = 1) +
+  geom_line() +
   geom_text(nudge_x = 0.15) +
   scale_x_continuous(breaks = 1:10) +
   scale_y_comma(expand_min = 0.05) +
@@ -168,11 +215,10 @@ ratings |>
   filter(episode_type != 'Middle') |> 
   select(episode_type, series, total_viewers_m) |> 
   ggplot(aes(series, total_viewers_m, colour = episode_type, fill = episode_type)) +
-  geom_line(linewidth = 1) +
-  geom_point(shape = 21, stroke = 1, colour = 'white') +
+  geom_line() +
+  geom_point() +
   scale_x_continuous(breaks = 1:10) +
   scale_y_comma(expand_min = 0.05) +
-  scale_fill_manual(values = type_pal) +
   scale_colour_manual(values = type_pal) +
   labs(
     x = 'Series', y = 'Total viewers (m)',
@@ -183,20 +229,93 @@ ratings |>
 
 #--- first last dumbbell ---
 
+ratings |> 
+  filter(episode_type != 'Middle') |> 
+  ggplot(aes(y = series, x = total_viewers_m, group = series, colour = episode_type)) +
+  geom_line() +
+  geom_point() +
+  scale_x_comma() +
+  scale_y_reverse(breaks = 1:nrow(ratings_first_last)) +
+  scale_colour_manual(values = type_pal) +
+  labs(
+    y = 'Series',
+    x = 'Total viewers (m)',
+    colour = NULL,
+    title = 'Change in viewership between Premiere and Finale'
+  )
+
 #--- first last slope ---
-
-#--- finale bumps ---
-
-#--- percentage change ---
-
-#--- percentage change lollipop ---
-
-#--- first last scatterplot ---
 
 ratings |> 
   filter(episode_type != 'Middle') |> 
-  select(episode_type, series, total_viewers_m) |> 
-  spread(key = episode_type, value = total_viewers_m) |> 
+  ggplot(aes(x = fct_reorder(episode_type, episode), y = total_viewers_m, 
+    colour = factor(series), group = factor(series), 
+    label = case_when(episode_type == 'Finale' ~ series))) +
+  geom_line() +
+  geom_point() +
+  geom_text(hjust = -0.75) +
+  scale_y_comma() +
+  scale_colour_series() +
+  labs(
+    x = 'Episode',
+    y = 'Total viewers (m)',
+    colour = NULL,
+    title = 'Change in viewership between Premiere and Finale'
+  ) +
+  guides(colour = 'none')
+  
+
+#--- finale bumps ---
+
+ratings_first_last |> 
+  ggplot(aes(series, num_change)) +
+  geom_col() +
+  scale_x_reverse(breaks = 1:nrow(ratings_first_last)) +
+  scale_y_comma(expand_min = 0.05) +
+  labs(
+    y = 'Difference in total viewers (m)',
+    x = 'Series',
+    title = "No 'Finale Bump' pattern across series",
+    subtitle = 'Change in viewership between Premiere and Finale'
+  ) +
+  coord_flip()
+
+#--- percentage change ---
+
+ratings_first_last |> 
+  ggplot(aes(series, pct_change)) +
+  geom_col() +
+  geom_hline(yintercept = median(ratings_first_last$pct_change)) +
+  scale_x_reverse(breaks = 1:nrow(ratings_first_last)) +
+  scale_y_continuous(label = percent) +
+  labs(
+    y = '% Difference in total viewers',
+    x = 'Series',
+    title = "No 'Finale Bump' pattern across series",
+    subtitle = 'Percent change in viewership between Premiere and Finale'
+  ) +
+  coord_flip()
+
+#--- percentage change lollipop ---
+
+ratings_first_last |> 
+  ggplot(aes(pct_change, series, label = label, xend = 0)) +
+  geom_vline(xintercept = 0) +
+  geom_text(aes(hjust = -1 *sign(pct_change) + 0.5)) +
+  geom_point() +
+  geom_segment() +
+  scale_x_continuous(label = percent) +
+  scale_y_reverse(breaks = 1:nrow(ratings_first_last)) +
+  labs(
+    x = '% change',
+    y = 'Series',
+    title = 'Series 3 had the biggest increase between Premiere and Finale',
+    subtitle = 'Change in viewership between Premiere and Finale'
+  )
+
+#--- first last scatterplot ---
+
+ratings_first_last |> 
   ggplot(aes(Premiere, Finale, label = series, colour = factor(series))) +
   geom_point() +
   geom_text(nudge_x = 0.1) +
